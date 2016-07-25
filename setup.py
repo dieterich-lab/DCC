8312a9f59f31b05fa23a5160c53ec65f25573fad
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2016-07-25 12:52:24 +0200

    Version bump to 0.4.3.

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup
-# To use a consistent encoding
 from codecs import open
 from os import path
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -23,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.2',
+    version='0.4.3',
 
     description='Detect circRNAs from chimeras',
     long_description=long_description,
