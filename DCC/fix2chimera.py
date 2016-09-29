215548334d8d3a12dab9d017432b92008da2a6e4
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2016-09-29 17:12:01 +0200

    We're now testing the length of the line_split variable instead of the line which does not make sense.

--- DCC/fix2chimera.py
+++ DCC/fix2chimera.py
@@ -49,8 +49,10 @@ class Fix2Chimera(object):
         output = open(output_file, 'w')
         for line in chimeric_junction:
 
+            line_split = line.split('\t')
+
             # check if the row has all fields
-            if len(line) < 14:
+            if len(line_split) < 14:
                 print ("WARNING: File " + str(chimeric_junction_mate2) + ", line " + str(linecnt)
                        + " does not contain all features.")
                 print ("WARNING: " + str(chimeric_junction_mate2) + " is probably corrupt.")
@@ -58,7 +60,6 @@ class Fix2Chimera(object):
 
             linecnt += linecnt
 
-            line_split = line.split('\t')
             if line_split[2] == '+':
                 line_split[2] = '-'
                 line_split[5] = '-'
